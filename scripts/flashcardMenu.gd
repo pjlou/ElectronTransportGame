623dd2596fcd1bf6384cf882e17f6dcc3ff28d96
patrick <pl2@uab.edu>
2026-03-16 12:19:58 -0500

    fix flashcard menu navigation

--- scripts/flashcardMenu.gd
+++ scripts/flashcardMenu.gd
@@ -4,36 +4,7 @@ var buttons := []
 var selected_button = null
 
 func _ready() -> void:
-	buttons = [
-		$startButton,
-		$settingsButton,
-		$viewButton,
-		$editButton,
-	]
-	for button in buttons:
-		button.toggle_mode = true
-		button.toggled.connect(func(pressed): _on_button_toggled(pressed, button))
-	$startButton.set_meta("scene_path", "res://scenes/flashcardMode.tscn")
-	$settingsButton.set_meta("scene_path", "res://scenes/flashcardSettings.tscn")
-	$viewButton.set_meta("scene_path", "res://scenes/flashcardView.tscn")
-	$editButton.set_meta("scene_path", "res://scenes/flashcardEdit.tscn")
-
-func _on_button_toggled(button_pressed: bool, toggled_button):
-	if button_pressed:
-		for b in buttons:
-			if b != toggled_button:
-				b.button_pressed = false
-		selected_button = toggled_button
-		print("Selected:", toggled_button.name)
-		$playButton.disabled = false
-
-func _on_play_button_pressed():
-	if selected_button:
-		var scene_path = selected_button.get_meta("scene_path")
-		print("Scene path selected:", scene_path)
-		get_tree().change_scene_to_file(scene_path)
-	else:
-		print("No button selected")
+	pass
 
 func _on_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/modeSelection.tscn")
@@ -42,6 +13,8 @@ func _on_menu_button_pressed() -> void:
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/leaderboard.tscn")
 
-
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/flashcardMode.tscn")
+
+func _on_settings_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/flashcardSettings.tscn")
